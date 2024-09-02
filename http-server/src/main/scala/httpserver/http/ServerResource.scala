6627cff6827eb79360a0f572cc0a618dbe36f617
Matt Moore <matt@mattmoore.io>
2024-09-01 22:13:01 -0400

    Move routes into separate files

--- http-server/src/main/scala/httpserver/http/ServerResource.scala
+++ http-server/src/main/scala/httpserver/http/ServerResource.scala
@@ -1,18 +1,16 @@
 package httpserver.http
 
 import cats.*
+import cats.data.Kleisli
+import cats.data.OptionT
 import cats.effect.*
 import cats.implicits.*
 import com.comcast.ip4s.*
 import fs2.io.net.Network
 import httpserver.domain.*
+import httpserver.http.routes.*
 import httpserver.services.*
-import io.circe.*
-import io.circe.generic.auto.*
-import io.circe.syntax.*
 import org.http4s.*
-import org.http4s.circe.CirceEntityDecoder.*
-import org.http4s.circe.*
 import org.http4s.dsl.*
 import org.http4s.ember.server.*
 import org.http4s.implicits.*
@@ -25,48 +23,24 @@ object ServerResource {
       logger: Logger[F],
       helloService: HelloService[F],
       geolocationService: GeolocationService[F],
-  ): Resource[F, Server] =
-    val dsl = Http4sDsl[F]
-    import dsl.*
+  ): Resource[F, Server] = {
+    given Http4sDsl[F] = Http4sDsl[F]
 
-    val helloRoutes = HttpRoutes.of[F] {
-      case GET -> Root / "hello" / name =>
-        helloService
-          .hello(name)
-          .flatMap(Ok(_))
-          .handleErrorWith(_ => InternalServerError())
-      case GET -> Root / "healthcheck" =>
-        Ok()
-    }
-
-    val geolocationRoutes = HttpRoutes.of[F] {
-      case req @ POST -> Root / "coords" =>
-        for {
-          request <- req.as[requests.CoordsRequest]
-          addressQuery = AddressQuery(
-            street = request.street,
-            city = request.city,
-            state = request.state,
-          )
-          response <- geolocationService
-            .getCoords(addressQuery)
-            .flatMap {
-              case Right(coords) => Ok(coords.asJson)
-              case Left(error)   => Ok(error)
-            }
-            .handleErrorWith(_ => InternalServerError())
-        } yield (response)
-      case GET -> Root / "healthcheck" =>
-        Ok()
-    }
-
-    val allRoutes = (helloRoutes <+> geolocationRoutes).orNotFound
+    val routes: HttpRoutes[F] = List(
+      HelloRoutes(),
+      GeolocationRoutes(),
+    ).foldK
 
     EmberServerBuilder
       .default[F]
       .withHost(ipv4"0.0.0.0")
-      .withPort(Port.fromInt(config.port).getOrElse(port"8080"))
-      .withHttpApp(allRoutes)
+      .withPort(
+        Port
+          .fromInt(config.port)
+          .getOrElse(port"8080"),
+      )
+      .withHttpApp(routes.orNotFound)
       .withLogger(logger)
       .build
+  }
 }
