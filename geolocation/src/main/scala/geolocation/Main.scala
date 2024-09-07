c1e3016c4ef995d96f411cc1943795cdfaf93bbd
Matt Moore <matt@mattmoore.io>
2024-09-06 22:30:18 -0400

    Tweak Main.scala

--- geolocation/src/main/scala/geolocation/Main.scala
+++ geolocation/src/main/scala/geolocation/Main.scala
@@ -1,22 +1,9 @@
 package geolocation
 
 import cats.effect.*
+import cats.syntax.all.*
 
-// Can run this as a ResourceApp.Forever with a for comprehension
 object Main extends ResourceApp.Forever {
-  def run(args: List[String]): Resource[IO, Unit] = for {
-    _ <- Resources.make[IO]
-  } yield ()
+  def run(args: List[String]): Resource[IO, Unit] =
+    Resources.make[IO].void
 }
-
-// Can run this as a ResourceApp.Forever with a map
-// object Main extends ResourceApp.Forever {
-//   def run(args: List[String]): Resource[IO, Unit] =
-//     Resources.make[IO].map(_ => ())
-// }
-
-// Can run this as an IOApp with a useForever
-// object Main extends IOApp {
-//   def run(args: List[String]): IO[ExitCode] =
-//     Resources.make[IO].useForever.as(ExitCode.Success)
-// }
