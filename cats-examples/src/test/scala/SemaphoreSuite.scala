a52499f1892d1fa84fd29a9a0725476e50f2f493
Matt Moore <matt@mattmoore.io>
2024-09-10 18:06:37 -0400

    Add more semaphore examples with parTraverse

--- cats-examples/src/test/scala/SemaphoreSuite.scala
+++ cats-examples/src/test/scala/SemaphoreSuite.scala
@@ -1,7 +1,5 @@
-import cats.effect.IO
-import cats.effect.Temporal
-import cats.effect.std.Console
-import cats.effect.std.Semaphore
+import cats.effect.*
+import cats.effect.std.*
 import cats.effect.syntax.all.*
 import cats.implicits.*
 import weaver.*
@@ -9,8 +7,10 @@ import weaver.*
 import scala.concurrent.duration.*
 
 object SemaphoreSuite extends SimpleIOSuite {
-  test("Semaphore is useful for throttling - when multiple processes try to access a resource and we want to constrain the number of accesses") {
-    class PreciousResource[F[_]: Temporal](name: String, s: Semaphore[F])(implicit F: Console[F]) {
+  type F[A] = IO[A]
+
+  test("From cats effect website example: Semaphore is useful for throttling - when multiple processes try to access a resource and we want to constrain the number of accesses") {
+    class PreciousResource[F[_]: Temporal](name: String, s: Semaphore[F])(using F: Console[F]) {
       def use: F[Unit] =
         for {
           x <- s.available
@@ -31,5 +31,64 @@ object SemaphoreSuite extends SimpleIOSuite {
       r3 = new PreciousResource[IO]("R3", s)
       result <- List(r1.use, r2.use, r3.use).parSequence.void
     } yield expect(result == ())
+  }
+
+  test("Semaphore example with parTraverse") {
+    case class PersonRow(id: Int, firstName: String, lastName: String)
+
+    final class DataRepository[F[_]: Temporal](store: AtomicCell[F, List[PersonRow]], s: Semaphore[F])(using F: Console[F]) {
+      def putPerson(person: PersonRow): F[Unit] =
+        for {
+          _ <- s.acquire
+          x <- s.available
+          _ <- F.println(s"DataRepository availability: $x")
+          _ <- store.update(person +: _)
+          _ <- s.release
+          y <- s.available
+          _ <- F.println(s"DataRepository availability: $y")
+        } yield ()
+    }
+
+    val people = List.tabulate(10)(n => PersonRow(n + 1, "Matt", "Moore"))
+
+    // To demonstrate that in spite of calling parTraverseN, we're limiting to 1 worker thread, so only one fiber gets executed at a time
+    val WorkerThreadCount = 1
+
+    for {
+      table     <- AtomicCell[F].of(List.empty[PersonRow])
+      semaphore <- Semaphore[F](WorkerThreadCount)
+      repo = new DataRepository(table, semaphore)
+      rowsBefore <- table.get
+      _          <- people.parTraverse(repo.putPerson).void
+      rowsAfter  <- table.get
+    } yield expect.all(
+      rowsBefore == List.empty,
+      rowsAfter.sortBy(_.id) == people,
+    )
+  }
+
+  test("Semaphore example with parTraverseN - parTraverseN automatically utilizes a semaphore, so we don't have to handle it") {
+    case class PersonRow(id: Int, firstName: String, lastName: String)
+
+    final class DataRepository[F[_]: Temporal](store: AtomicCell[F, List[PersonRow]]) {
+      def putPerson(person: PersonRow): F[Unit] =
+        store.update(person +: _)
+    }
+
+    val people = List.tabulate(10)(n => PersonRow(n + 1, "Matt", "Moore"))
+
+    // To demonstrate that in spite of calling parTraverseN, we're limiting to 1 worker thread, so only one fiber gets executed at a time
+    val WorkerThreadCount = 1
+
+    for {
+      table <- AtomicCell[F].of(List.empty[PersonRow])
+      repo = new DataRepository[F](table)
+      rowsBefore <- table.get
+      _          <- people.parTraverseN(WorkerThreadCount)(repo.putPerson).void
+      rowsAfter  <- table.get
+    } yield expect.all(
+      rowsBefore == List.empty,
+      rowsAfter.sortBy(_.id) == people,
+    )
   }
 }
